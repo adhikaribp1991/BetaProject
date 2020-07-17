e98d8fd3e76f03ac49ce8e56945e408fbb6c878a
adhikaribp1991 <adhikaribp1991@gmail.com>
2020-07-17 16:37:29 -0400

    new package added git status

--- src/Package1/Class01.java
+++ src/Package1/Class01.java
@@ -3,5 +3,6 @@ package Package1;
 public class Class01 {
 public static void main(String[] args) {
 	System.out.println("Hello World");
+	System.out.println("This is a change");
 }
 }
---
+++ src/Package2/Class2.java
@@ -0,0 +1,7 @@
+package Package2;
+
+public class Class2 {
+public static void main(String[] args) {
+	System.out.println("This is class 2");
+}
+}
